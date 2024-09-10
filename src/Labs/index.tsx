2ab433607ac63928fd0e87945f9891adbd4d3b8e
Ninesay <xul28@uci.edu>
2024-09-09 21:11:58 -0700

    added name on Labs/index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,8 +1,9 @@
+// Xu Li
 import Lab1 from "./Lab1";
-import { Route, Routes, Navigate } from "react-router";
-import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import TOC from "./TOC";
+import { Route, Routes, Navigate } from "react-router";
 
 export default function Labs() {
     return (
