7b11fe86a961f648f1870d75aa64b1966fa8579b
Ninesay <xul28@uci.edu>
2024-10-03 19:07:10 -0700

    add some contents to Modules page

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -9,41 +9,144 @@ export default function Modules() {
             <ModulesControls/><br/><br/><br/><br/>
             <ul id="wd-modules" className="list-group rounded-0">
                 <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-                    <div className="wd-title p-3 ps-2 bg-secondary">
-                        <BsGripVertical className="me-2 fs-3" />
-                        Week 1
+                    <div className="wd-title p-3 ps-2 bg-secondary d-flex justify-content-between align-items-center">
+                        <div>
+                            <BsGripVertical className="me-2 fs-3" />
+                            Week 1: Introduction to Web Development
+                        </div>
                         <ModuleControlButtons />
                     </div>
                     <ul className="wd-lessons list-group rounded-0">
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <BsGripVertical className="me-2 fs-3" />
-                            LEARNING OBJECTIVES
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                LEARNING OBJECTIVES
+                            </div>
                             <LessonControlButtons />
                         </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            <BsGripVertical className="me-2 fs-3" />
-                            Introduction to the course
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                1.1 What is Web Development?
+                            </div>
                             <LessonControlButtons />
                         </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            Learn what is Web Development
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                1.2 The Internet and How It Works
+                            </div>
+                            <LessonControlButtons />
                         </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1</li>
-                        <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2</li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                1.3 Introduction to HTML
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                A1 - ENV + HTML
+                            </div>
+                            <LessonControlButtons />
+                        </li>
                     </ul>
                 </li>
                 <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-                    <div className="wd-title p-3 ps-2 bg-secondary"> Week 2</div>
+                    <div className="wd-title p-3 ps-2 bg-secondary d-flex justify-content-between align-items-center">
+                        <div>
+                            <BsGripVertical className="me-2 fs-3" />
+                            Week 2: CSS and Web Design Principles
+                        </div>
+                        <ModuleControlButtons />
+                    </div>
                     <ul className="wd-lessons list-group rounded-0">
-                        <li className="wd-lesson list-group-item p-3 ps-1">
-                            LEARNING OBJECTIVES
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                LEARNING OBJECTIVES
+                            </div>
+                            <LessonControlButtons />
                         </li>
-                        <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1</li>
-                        <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2</li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                2.1 Introduction to CSS
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                2.2 CSS Box Model and Layout
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                2.3 Responsive Design and Media Queries
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                A2 - CSS + BOOTSTRAP
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                    </ul>
+                </li>
+                <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+                    <div className="wd-title p-3 ps-2 bg-secondary d-flex justify-content-between align-items-center">
+                        <div>
+                            <BsGripVertical className="me-2 fs-3" />
+                            Week 3: JavaScript and React Basics
+                        </div>
+                        <ModuleControlButtons />
+                    </div>
+                    <ul className="wd-lessons list-group rounded-0">
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                LEARNING OBJECTIVES
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                3.1 Introduction to JavaScript
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                3.2 React Fundamentals
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                3.3 Components and Props
+                            </div>
+                            <LessonControlButtons />
+                        </li>
+                        <li className="wd-lesson list-group-item p-3 ps-1 d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsGripVertical className="me-2 fs-3" />
+                                A3 - JAVASCRIPT + REACT
+                            </div>
+                            <LessonControlButtons />
+                        </li>
                     </ul>
                 </li>
             </ul>
         </div>
-
     );
 }
