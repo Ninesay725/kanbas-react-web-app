59f7468b3f28da21d0d1f4f5a1b23eae7754c115
Ninesay <xul28@uci.edu>
2024-10-16 19:27:25 -0700

    create tab Lab4

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+
+function Lab4() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default Lab4;
