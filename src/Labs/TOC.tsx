7dd66872b5a0449c2fba18516e4898b32b47f4f6
Ninesay <xul28@uci.edu>
2024-12-07 23:09:53 -0800

    Update TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -63,7 +63,7 @@ export default function TOC() {
             </li>
             <li className="nav-item">
                 <a id="wd-node-server" 
-                    href="https://kanbas-node-server-app-1-0nva.onrender.com" 
+                    href="https://kanbas-node-server-app-2-zaxa.onrender.com" 
                     target="_blank"
                     className="nav-link">
                     Node Server Root
